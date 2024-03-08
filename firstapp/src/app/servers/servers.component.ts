97de9bcd0be67173ff9369bfce382d6ffc77ff72
DicomHamzaOzturk <hamzaozturk@dicom-software.de>
2024-03-08 11:56:57 +0100

    3

--- firstapp/src/app/servers/servers.component.ts
+++ firstapp/src/app/servers/servers.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  styleUrl: './servers.component.css',
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverStatus = 'offline';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
 }
